9e0dd9687fa78f0bdb17c610665eb9c0244ade5b
Mario2930 <100131163+Mario2930@users.noreply.github.com>
2022-06-23 13:19:25 -0300

    Add files via upload

---
+++ ac.js
@@ -0,0 +1,23 @@
+
+class McLanche_Alegre {
+    #fruta
+    constructor(carne, pao, fruta){
+        this.fruta =fruta;
+        this.carne = carne;
+        this.pao = pao;
+
+    }
+class Big_Lanche extends McLanche_Alegre {
+    constructor(carne, pao, outros_ingredientes, acompanhamentos);
+    this.carne = carne+carne;
+    this.pao = pao;
+    this.outros_ingredientes = 4;
+    this.acompanhamentos = 2;
+}
+class McHen extends McLanche_Alegre{
+    constructor(pao, outros_ingredientes, acompanhamentos, frango);
+    this.pao = pao;
+    this.outros_ingredientes = 3;
+    this.frango = frango;
+    this.acompanhamentos = 2;
+}
